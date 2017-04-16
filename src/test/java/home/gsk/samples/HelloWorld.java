75f65ca76b2fb8f81f4709ddd9cd6cfb79636582
Sivakumar Ganesan <Sivakumar Ganesan>
2017-04-15 22:18:21 -0400

    first commit

---
+++ src/test/java/home/gsk/samples/HelloWorld.java
@@ -0,0 +1,13 @@
+package home.gsk.samples;
+
+/**
+ * Created by Sivak
+ * on 4/14/2017.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/home/gsk/samples/PrintTrianglePattern.java
@@ -0,0 +1,32 @@
+package home.gsk.samples;
+
+import java.util.Scanner;
+
+/**
+ * Created by Sivak
+ * on 4/14/2017.
+ */
+public class PrintTrianglePattern
+{
+    public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter required number of rows: ");
+        int num = reader.nextInt();
+        printPattern(num);
+    }
+
+    public static void printPattern(int numRows)
+    {
+        for(int i=0; i<numRows; i++)
+        {
+            int j=i;
+            if(i<=j)
+            {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println("\n");
+        }
+    }
+}
